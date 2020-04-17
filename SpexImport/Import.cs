3e984966605f270fbda106bc9846b956118f65bc
CWynne-DHE <brian.wynne@dhecs.com>
2020-04-17 13:38:19 -0600

    Add project files.

---
+++ SpexImport/Import.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.IO.Compression;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpexImport
+{
+    class Import
+    {
+        static void Main(string[] args)
+        {
+            UnzipCatalogContents();
+            System.Environment.Exit(0);
+        }
+
+        static void DownloadFromFTP()
+        {
+
+        }
+
+        static void UnzipCatalogContents()
+        {
+            string zipPath = @".\basic.zip";
+            string extractPath = @".\spex";
+
+            ZipFile.ExtractToDirectory(zipPath, extractPath);
+        }
+    }
+}
