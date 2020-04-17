7bbc441327dd1db9c5223f8b9deaa6ca5adbe7e1
CWynne-DHE <brian.wynne@dhecs.com>
2020-04-17 14:14:58 -0600

    FTP download + unzip

--- SpexImport/Import.cs
+++ SpexImport/Import.cs
@@ -13,12 +13,35 @@ namespace SpexImport
     {
         static void Main(string[] args)
         {
-            UnzipCatalogContents();
+            DownloadFromFTP();
             System.Environment.Exit(0);
         }
 
         static void DownloadFromFTP()
         {
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://ftp.etilize.com/IT_CE/content/EN_US/basic/basic_EN_US_current_mysql.zip");
+            request.Method = WebRequestMethods.Ftp.DownloadFile;
+
+            request.Credentials = new NetworkCredential("dhecsdelta", "8q@Hsb3lta");
+
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+            Stream responseStream = response.GetResponseStream();
+            FileStream file = File.Create(@"basic.zip");
+            byte[] buffer = new byte[32 * 1024];
+            int read;
+            
+            while ((read = responseStream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                file.Write(buffer, 0, read);
+            }
+
+            Console.WriteLine($"Download complete, status {response.StatusDescription}");
+
+            file.Close();
+            response.Close();
+
+            UnzipCatalogContents();
         }
 
         static void UnzipCatalogContents()
