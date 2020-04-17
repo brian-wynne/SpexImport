484d32cfd1d4d473c3455c15e4aaf895f67cf4f5
CWynne-DHE <brian.wynne@dhecs.com>
2020-04-17 14:50:44 -0600

    Basic SQL

--- SpexImport/Import.cs
+++ SpexImport/Import.cs
@@ -6,20 +6,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Npgsql;
+using Microsoft.VisualBasic.FileIO;
 
 namespace SpexImport
 {
     class Import
     {
+        private static string db_host = "localhost";
+        private static string db_user = "spex";
+        private static string db_pass = "spex";
+        private static string db_name = "Spex";
+        private static string db_port = "5432";
+
         static void Main(string[] args)
         {
-            DownloadFromFTP();
+            //DownloadFromFTP("ftp://ftp.etilize.com/IT_CE/content/EN_US/basic/basic_EN_US_current_mysql.zip");
+            ConnectToDatabase();
             System.Environment.Exit(0);
         }
 
-        static void DownloadFromFTP()
+        static void DownloadFromFTP(string url)
         {
-            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://ftp.etilize.com/IT_CE/content/EN_US/basic/basic_EN_US_current_mysql.zip");
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(url);
             request.Method = WebRequestMethods.Ftp.DownloadFile;
 
             request.Credentials = new NetworkCredential("dhecsdelta", "8q@Hsb3lta");
@@ -50,6 +59,53 @@ namespace SpexImport
             string extractPath = @".\spex";
 
             ZipFile.ExtractToDirectory(zipPath, extractPath);
+
+            Console.WriteLine("Unzipped basic.zip successfully");
+
+            ConnectToDatabase();
+        }
+
+        static void ConnectToDatabase()
+        {
+            string connString = String.Format("Server={0};Username={1};Database={2};Port={3};Password={4};SSLMode=Prefer",
+                db_host,
+                db_user,
+                db_name,
+                db_port,
+                db_pass);
+
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                Console.WriteLine("Establishing connection to PostgreSQL Database");
+                conn.Open();
+
+                using (var command = new NpgsqlCommand("DROP TABLE IF EXISTS products", conn))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                using (var command = new NpgsqlCommand("CREATE TABLE products (product_id VARCHAR(32) NOT NULL, manufacture_id VARCHAR(32), is_active boolean, mfg_pn VARCHAR(32) NOT NULL, category_id INT, is_accessory boolean, equivalency NUMERIC, create_date TIMESTAMP, modify_date TIMESTAMP, last_update TIMESTAMP, PRIMARY KEY(product_id, mfg_pn));", conn))
+                {
+                    command.ExecuteNonQuery();
+                    ImportProductsToDatabase();
+                }
+            }
+        }
+
+        static void ImportProductsToDatabase()
+        {
+            string file = @".\spex\EN_US_B_product.csv";
+
+            using (TextFieldParser parser = new TextFieldParser(file))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+
+                while (!parser.EndOfData)
+                {
+                    string[] fields = parser.ReadFields();
+                }
+            }
         }
     }
 }
