8d0a425619a1c2a1cfd2a91f6acee39ea1d430c4
brian-wynne <brianwynne24@gmail.com>
2020-08-31 09:28:07 -0600

    Added foreign key for accessoryid -> (productid)

--- SpexImport/scripts/keys/productaccessories.sql
+++ SpexImport/scripts/keys/productaccessories.sql
@@ -1,3 +1,4 @@
 ALTER TABLE productaccessories_temp
 	ADD FOREIGN KEY(productid) REFERENCES product(productid),
+	ADD FOREIGN KEY(accessoryid) REFERENCES product(productid),
 	ADD FOREIGN KEY(localeid) REFERENCES locales(localeid);
